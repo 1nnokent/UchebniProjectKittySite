db12ae1270e5e65dbe5abefae8e9a0e220d74b11
1nnokent <maxlynch2007@gmail.com>
2025-01-26 23:37:19 +0300

    добавил декоратор для learning_materials, database_requests сокращено до dr

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 import sqlite3 as sq
 from config import database
-import database_requests
+import database_requests as dr
 
 app = Flask(__name__, template_folder="templates")
 
@@ -18,7 +18,7 @@ def registration_page():
     if request.method == "GET":
         return render_template("/user_account_pages/registration.html")
     elif request.method == "POST":
-        code = database_requests.insert_user(request.form.to_dict())
+        code = dr.insert_user(request.form.to_dict())
         if code == 0:
             return render_template("/user_account_pages/registration_end.html")
         elif code == 1:
@@ -34,7 +34,7 @@ def authorization_page(failed=False, problem=None):
         return render_template("user_account_pages/authorization_failed.html", problem=problem)
     if request.method == 'POST':
         info = request.form.to_dict()
-        password_input = database_requests.get_password_with_login(info["login"])
+        password_input = dr.get_password_with_login(info["login"])
         if not password_input:
             return authorization_page(failed=True, problem="Пользователя с данным логином не существует")
             #Отобразить "Неправильный логин или пароль" [неправильный логин]
@@ -42,16 +42,16 @@ def authorization_page(failed=False, problem=None):
             return authorization_page(failed=True, problem="Введен неверный пароль")
             #Отобразить "Неправильный логин или пароль" [неправильный пароль]
         else:
-            person_id = database_requests.get_user_id_with_login(info['login'])
+            person_id = dr.get_user_id_with_login(info['login'])
             return redirect(url_for('personal_user_page', user_id=person_id))
 
 @app.route("/users/<user_id>")
 def personal_user_page(user_id):
-    info = database_requests.get_user_info_with_user_id(user_id)
+    info = dr.get_user_info_with_user_id(user_id)
     if not info:
         return render_template("error_pages/authorization_user_not_found_error.html")
     else:
-        kwargs = database_requests.user_select_to_dict(info)
+        kwargs = dr.user_select_to_dict(info)
         return render_template("user_account_pages/user.html", **kwargs)
 
 @app.route("/problems/add", methods=['POST', 'GET'])
@@ -63,14 +63,20 @@ def add_problem():
         if not (1 <= int(info['problem_type']) <= 27):
             return render_template("error_incorrect_problem") #Такого номера задания нет в КИМ
 
-        database_requests.insert_problem(int(info['problem_type']), info['problem_class'], info['problem_source'],
+        dr.insert_problem(int(info['problem_type']), info['problem_class'], info['problem_source'],
                                          info['problem_statement'], info['problem_answer'], int(info['problem_difficulty']))
         return render_template('problem_added.html')
 
 @app.route("/test")
 def test_page():
-    problems = database_requests.sql_execute(f"""SELECT * FROM problems""").fetchall()
+    problems = dr.sql_execute(f"""SELECT * FROM problems""").fetchall()
     return render_template("test1.html", problems=problems, user=[0, "aowje"])
+
+@app.route("/learning_materials/list")
+def learning_materials_page():
+    materials = dr.get_learning_materials()
+    return render_template('learning_materials.html', materials=materials)
+
 
 @app.route("/blank")
 def blank_page():
@@ -79,16 +85,16 @@ def blank_page():
 @app.route("/variants/<variant_id>", methods=['GET', 'POST'])
 def variant_page(variant_id):
     if request.method == 'GET':
-        kwargs = database_requests.variant_page_default_kwargs(variant_id)
+        kwargs = dr.variant_page_default_kwargs(variant_id)
         return render_template("variant_page.html", **kwargs)
     elif request.method == 'POST':
-        kwargs = database_requests.variant_page_feedback_kwargs(variant_id)
-        database_requests.insert_variant_answers(request.form.to_dict(), variant_id, -1, -1)
+        kwargs = dr.variant_page_feedback_kwargs(variant_id)
+        dr.insert_variant_answers(request.form.to_dict(), variant_id, -1, -1)
         return render_template("variant_page.html", **kwargs)
 
 @app.route("/problems/list")
 def problems_page():
-    problems = database_requests.sql_execute(f"""SELECT * FROM problems""").fetchall()
+    problems = dr.sql_execute(f"""SELECT * FROM problems""").fetchall()
     return render_template("problem_list.html", problems=problems)
 
 if __name__ == "__main__":
